9e9642f3f6fc06b0083ebab4b1f5a33a22dc4fc1
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2023-10-16 19:52:52 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
-import Card from './src/component/TinderCard/index.js';
-import users from './assets/data/users.js'
-const jeff ={
-  name: 'Jeff',
-  bio: 'I m Jeff',
-  image:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
-}
+
+import HomeScreen from './src/screens/HomeScreen';
+import MatchesScreen from './src/screens/MatchesScreen';
+
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <Card user={jeff} />
+      <MatchesScreen />
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   pageContainer: {
     justifyContent: 'center',
@@ -21,4 +20,5 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
 export default App;
--- src/component/TinderCard/index.js
+++ src/component/TinderCard/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
+import {Text, ImageBackground, View, StyleSheet} from 'react-native';
+
 const Card = props => {
   const {name, image, bio} = props.user;
   return (
@@ -10,50 +11,52 @@ const Card = props => {
         }}
         style={styles.image}>
         <View style={styles.cardInner}>
-          <Text style={styles.image}>{name}</Text>
+          <Text style={styles.name}>{name}</Text>
           <Text style={styles.bio}>{bio}</Text>
         </View>
       </ImageBackground>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
-    card: {
-      width: '100%',
-      height: '100%',
-      borderRadius: 10,
-      backgroundColor: '#fefefe',
-  
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 5,
-      },
-      shadowOpacity: 0.36,
-      shadowRadius: 6.68,
-  
-      elevation: 11,
+  card: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    backgroundColor: '#fefefe',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    image: {
-      width: '100%',
-      height: '100%',
-      borderRadius: 10,
-      overflow: 'hidden',
-  
-      justifyContent: 'flex-end',
-    },
-    cardInner: {
-      padding: 10,
-    },
-    name: {
-      fontSize: 30,
-      color: 'white',
-      fontWeight: 'bold',
-    },
-    bio: {
-      fontSize: 18,
-      color: 'white',
-      lineHeight: 25,
-    },
-  });
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  bio: {
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 25,
+  },
+});
+
 export default Card;
